80ed3c21937eea9333aea1a88aef1263cd048c71
JoelGo <joel.gotsch@gmail.com>
2020-10-13 11:02:56 +0200

    cleaned up a bit now working on workouts_screen and main

--- lib/views/workouts_screen.dart
+++ lib/views/workouts_screen.dart
@@ -1,44 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../theme/routes.dart';
 
-class AricleScreen extends StatefulWidget {
-  AricleScreen();
+class AddWorkoutScreen extends StatefulWidget {
+  final String apiKey;
+  AddWorkoutScreen({this.apiKey});
 
   @override
-  AricleScreenState createState() => AricleScreenState();
+  AddWorkoutScreenState createState() => AddWorkoutScreenState();
 }
 
-class AricleScreenState extends State<AricleScreen> {
-  AricleScreenState();
+class AddWorkoutScreenState extends State<AddWorkoutScreen> {
+  List<Workout> workouts = [];
+  WorkoutBloc workoutBloc;
+
+  @override
+  void initState() {
+    workoutBloc = WorkoutBloc(widget.apiKey);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // AddWorkoutScreenState();
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome to Flutter'),
-      ),
-      body: Center(
-        child: Text('Hello World'),
+    return Container(
+        color: Colors.grey, //darkGrey
+        child: StreamBuilder(
+          // Wrap our widget with a StreamBuilder
+          stream: workoutBloc.getWorkouts, // pass our Stream getter here
+          initialData: [], // provide an initial data
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot != null) {
+              if (snapshot.data.length > 0) {
+                return _buildListSimple(context, snapshot.data);
+              } else if (snapshot.data.length == 0) {
+                return Center(child: Text('No Data'));
+              }
+            } else if (snapshot.hasError) {
+              return Container();
+            }
+            return CircularProgressIndicator();
+          }, // access the data in our Stream here
+        )
+        // child: ReorderableListView(∆
+        //   padding: EdgeInsets.only(top: 300),
+        //   children: todoItems,
+        //   onReorder: _onReorder,
+        // ),
+        );
+  }
+
+  Widget _buildListSimple(BuildContext context, List<Workout> workoutList) {
+    return Theme(
+      data: ThemeData(canvasColor: Colors.transparent),
+      child: ListView(
+        padding: EdgeInsets.only(top: 300.0),
+        children: workoutList
+            .map((Workout item) => _buildListTile(context, item))
+            .toList(),
       ),
     );
   }
+
+  Widget _buildListTile(BuildContext context, Workout item) {
+    return ListTile(
+      leading: Icon(Icons.account_circle),
+      key: Key(item.workoutId.toString()),
+      title: Text(DateFormat('EEE, MMM d').format(item.date)),
+      onTap: () => Navigator.of(context).pushNamed(AppRoutes.menu),
+      subtitle: generateSubtitleWorkout(item),
+      isThreeLine: true,
+      dense: true,
+    );
+  }
+
+  Text generateSubtitleWorkout(wo) {
+    return (Text(""));
+  }
+
+  Widget _workoutTitle(BuildContext context, Workout wo) {}
 }
-
-
-
-
 
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:flutter/material.dart';
 // import 'package:url_launcher/url_launcher.dart';
 // import 'package:cached_network_image/cached_network_image.dart';
 
-// class AricleScreen extends StatefulWidget {
-//   AricleScreen();
+// class AddWorkoutScreen extends StatefulWidget {
+//   AddWorkoutScreen();
 
 //   @override
-//   AricleScreenState createState() => AricleScreenState();
+//   AddWorkoutScreenState createState() => AddWorkoutScreenState();
 // }
 
-// class AricleScreenState extends State<AricleScreen> {
-//   AricleScreenState();
+// class AddWorkoutScreenState extends State<AddWorkoutScreen> {
+//   AddWorkoutScreenState();
 
 //   _launchURL(String url) async {
 //     if (await canLaunch(url)) {
